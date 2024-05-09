ad4d943a7db42d80689ce00b444bb98e701fe219
capsuleismail <81362009+capsuleismail@users.noreply.github.com>
2024-05-09 12:20:46 +0100

    Update filtering_values_for_election.py

--- filtering_values_for_election.py
+++ filtering_values_for_election.py
@@ -1,3 +1,19 @@
+# A specialist of statistics was hired to do an investigation about the people's voting intention for a certain candidate for the next elections.
+
+# Unfortunately, some of the interviewers, hired for the researches, are not honest and they want to modify the results because, some of them like the candidate too much, or others hate him.
+
+# The specialist suspects about the situation and will apply his own method named: Recursive 2.5 Standard Deviation Filter.
+
+# He does not know who of them are cheating and obviously, he doesn´t know the minimum or maximum possible votes for this candidate.
+# Rules:
+# 1.The method works with the following steps explained below:
+# 2.The method calculates the mean,  μ, and the standard deviation, σ for the received data that has n elements.
+# 3.A new data is obtained discarding all the values that are not in the range [μ - 2.5σ, μ + 2.5σ] (strictly lower or higher than these extremes)
+# 4.The values of  μ and σ are recalculated and an all the values will be filtered by the new range with the updated values of  μ and σ and so on.
+# 5.He receives the data from the interviewers. The data is the number of votes of samples less than 500 people per neighbourhood of a county.
+
+
+
 def average(lst):
 
     return sum(lst) / len(lst)

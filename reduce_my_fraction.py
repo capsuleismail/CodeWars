f8b36afbec91f157fbf7a6e1255bdf7b73c9c28f
capsuleismail <81362009+capsuleismail@users.noreply.github.com>
2024-05-09 12:23:18 +0100

    Add files via upload

---
+++ reduce_my_fraction.py
@@ -0,0 +1,34 @@
+# Write a function which reduces fractions to their simplest form!
+# Fractions will be presented as an array/tuple (depending on the language) of strictly positive integers,
+# and the reduced fraction must be returned as an array/tuple:
+
+# input:   [numerator, denominator]
+# output:  [reduced numerator, reduced denominator]
+# example: [45, 120] --> [3, 8]
+# All numerators and denominators will be positive integers.
+
+# Note: This is an introductory Kata for a series... coming soon!
+
+
+import time
+
+start = time.time()
+print('START', start)
+
+
+def reduce_fraction(fraction):
+    first, second = fraction
+    starter = max(fraction)
+    while 0 < max(fraction) <= 2:
+        return (1, 1)
+    for i in range(1, int(starter // 2) + 1):
+        if (first % i == 0) and (second % i == 0):
+            num = int(first / i)
+            den = int(second / i)
+    return (num, den)
+
+
+print(reduce_fraction([45, 120]))
+#[45, 120] --> [3, 8]
+end = time.time()
+print('END:', end - start)

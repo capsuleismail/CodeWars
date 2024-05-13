5ad15bdd163e7130616454506c62d0f3768d4c19
capsuleismail <81362009+capsuleismail@users.noreply.github.com>
2024-05-13 22:32:43 +0100

    Update least_larger.py

--- least_larger.py
+++ least_larger.py
@@ -7,8 +7,18 @@
 # but the least largest is 5 (so return the index of 5 which is 3)
 # if you don't have any numbers greater than the number at the given index return -1
 
-def least_larger(array, i):
-    try:
-        return array.index(array[i] + 1)
-    except:
-        return -1
+def least_larger(arr, index): 
+    
+                
+    target = arr[index]
+    min_greater_index = -1
+    min_greater_value = float('inf')
+    
+    for i in range(len(arr)):
+        if i == index:
+            continue
+        if arr[i] > target and arr[i] < min_greater_value:
+            min_greater_index = i
+            min_greater_value = arr[i]
+    
+    return min_greater_index if min_greater_index != -1 else -1

2677a89ce85ccd8939a90f3b04e62aa55041ae14
capsuleismail <81362009+capsuleismail@users.noreply.github.com>
2024-07-04 08:24:03 +0100

    Create equable_triangle.py

---
+++ equable_triangle.py
@@ -0,0 +1,12 @@
+import math
+def equable_triangle(a,b,c):
+    if a + b <= c or a + c <= b or b + c <= a:
+        return False
+    
+    perimeter = a + b + c
+    
+    s = perimeter / 2
+    
+    area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+    
+    return area == perimeter

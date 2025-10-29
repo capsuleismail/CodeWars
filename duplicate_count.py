42f38ab66bff8f5fff4b1fc8b0a41f53125220d2
capsuleismail <81362009+capsuleismail@users.noreply.github.com>
2025-10-29 14:15:02 +0000

    Add duplicate_count function to identify duplicates
    Implement function to count duplicate characters in a string.

---
+++ duplicate_count.py
@@ -0,0 +1,10 @@
+def duplicate_count(text):
+    text = text.lower()
+    result = []
+    #return len([x for x in set(text.lower()) if text.lower().count(x)>1]) 
+    for i in range(len(text)):
+        if text.count(text[i]) > 1 and text[i] not in result:
+            print(text[i])
+            result.append(text[i])
+    return len(result)
+        
